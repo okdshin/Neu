f331e5484f87a2980d004b272a99b4d0dcdd478d
okdshin <kokuzen@gmail.com>
2015-11-04 01:03:22 +0900

    modify xor_softmax

--- example/xor_softmax/main.cpp
+++ example/xor_softmax/main.cpp
@@ -3,11 +3,8 @@
 #include <neu/layers_algorithm.hpp>
 #include <neu/kernel.hpp>
 #include <neu/activation_func/softmax_loss.hpp>
-#include <neu/activation_func/sigmoid.hpp>
 #include <neu/activation_func/rectifier.hpp>
-#include <neu/activation_func/identity.hpp>
 #include <neu/activation_layer.hpp>
-//#include <neu/learning_rate_gen/fixed_learning_rate_gen.hpp>
 #include <neu/learning_rate_gen/weight_decay_and_momentum.hpp>
 #include <neu/fully_connected_layer.hpp>
 #include <neu/layer.hpp>
@@ -42,7 +39,7 @@ int main(int argc, char** argv) {
 		.input_dim(input_dim).batch_size(batch_size)
 		.output_dim(10);
 	auto relu1_param = neu::make_activation_layer_parameter(fc1_param);
-	auto fc2_param = neu::make_fully_connected_layer_parameter(fc1_param)
+	auto fc2_param = neu::make_fully_connected_layer_parameter(relu1_param)
 		.output_dim(output_dim);
 	auto softmax_loss_param = neu::make_activation_layer_parameter(fc2_param);
 
@@ -51,17 +48,18 @@ int main(int argc, char** argv) {
 	auto constant_g = [](){ return 0.f; };
 
 	neu::scalar base_lr = 0.01;
-	neu::scalar momentum = 0.0;
+	neu::scalar momentum = 0.9;
 	neu::scalar weight_decay = 0.;
 
 	auto fc1 = neu::make_fully_connected_layer(fc1_param, fc12_g, constant_g,
 		neu::make_weight_decay_and_momentum(base_lr, momentum, weight_decay,
 			fc1_param.weight_dim(), fc1_param.bias_dim()));
-	auto relu1 = neu::make_activation_layer<neu::rectifier>(relu1_param);
+	auto relu1 = neu::make_activation_layer(relu1_param, neu::rectifier());
 	auto fc2 = neu::make_fully_connected_layer(fc2_param, fc12_g, constant_g,
 		neu::make_weight_decay_and_momentum(base_lr, momentum, weight_decay,
 			fc2_param.weight_dim(), fc2_param.bias_dim()));
-	auto softmax_loss = neu::make_activation_layer<neu::softmax_loss>(softmax_loss_param);
+	auto softmax_loss = neu::make_activation_layer(softmax_loss_param,
+		neu::softmax_loss(output_dim, batch_size));
 
 	auto layers = std::vector<neu::layer>{
 		std::ref(fc1),
@@ -71,29 +69,25 @@ int main(int argc, char** argv) {
 	};
 	std::ofstream error_log("error.txt");
 	std::ofstream output_log("output.txt");
-	for(auto i = 0u; i < 3000u; ++i) {
-		neu::layers_forward(layers, input);
-		auto output = layers.back().get_next_input();
-		neu::print(output_log, output, batch_size);
-		neu::gpu_vector error(output.size());
-		boost::compute::transform(output.begin(), output.end(),
-			teach.begin(), error.begin(), boost::compute::minus<neu::scalar>());
-		neu::layers_backward(layers, error);
-		fc1.update();
-		fc2.update();
 
-		neu::gpu_vector squared_error(error.size());
-		boost::compute::transform(error.begin(), error.end(),
-			error.begin(), squared_error.begin(),
-			boost::compute::multiplies<neu::scalar>());
-		auto error_sum = boost::compute::accumulate(
-			squared_error.begin(), squared_error.end(), 0.f);
-		error_log << i << " " << error_sum << std::endl;
+	auto buffer_size = neu::max_inoutput_size(layers);
+	neu::gpu_vector buffer1(buffer_size);
+	neu::gpu_vector buffer2(buffer_size);
+	for(auto i = 0u; i < 100u; ++i) {
+		auto output_range = neu::layers_forward(layers,
+			neu::to_range(input), buffer1, buffer2);
+		neu::print(output_log, output_range, output_dim);
+		neu::print(std::cout, output_range, output_dim);
+		auto error_range = output_range;
+		neu::calc_last_layer_delta(output_range, teach, error_range);
+		neu::print(error_log, error_range, output_dim);
+		auto prev_delta_range = neu::layers_backward(layers,
+			error_range, buffer1, buffer2);
+		neu::layers_update(layers);
 	}
+	auto output_range = neu::layers_forward(layers,
+		neu::to_range(input), buffer1, buffer2);
 	neu::print(teach); std::cout << "\n";
-	neu::print(layers.back().get_next_input());
-	/*
-	YAML::Emitter yaml_emitter;
-	print_yaml(yaml_emitter, layers);
-	*/
+	neu::print(std::cout, output_range, output_dim);
+	boost::compute::system::finish();
 }
