fd2527ba2dce8912d9613a60461701912cbcb3aa
okdshin <kokuzen@gmail.com>
2015-11-19 09:59:33 +0900

    remove useless indices

--- neu/convolution_layer/indices.hpp
+++ neu/convolution_layer/indices.hpp
@@ -11,12 +11,6 @@ namespace neu {
 		std::vector<cpu_indices> filter_indices_list_for_input(input_width*input_width);
 		std::vector<cpu_indices> output_indices_list_for_input(input_width*input_width);
 
-		std::vector<cpu_indices> filter_indices_list_for_output(output_width*output_width);
-		std::vector<cpu_indices> input_indices_list_for_output(output_width*output_width);
-
-		std::vector<cpu_indices> input_indices_list_for_filter(filter_width*filter_width);
-		std::vector<cpu_indices> output_indices_list_for_filter(filter_width*filter_width);
-
 		for(auto _or = 0; _or < output_width; ++_or) {
 			for(auto oc = 0; oc < output_width; ++oc) {
 				for(auto fr = 0; fr < filter_width; ++fr) {
@@ -28,23 +22,14 @@ namespace neu {
 							const auto input_index = ir*input_width+ic;
 							const auto output_index = _or*output_width+oc;
 							const auto filter_index = fr*filter_width+fc;
-							filter_indices_list_for_output[output_index].push_back(filter_index);
-							input_indices_list_for_output[output_index].push_back(input_index);
-
 							filter_indices_list_for_input[input_index].push_back(filter_index);
 							output_indices_list_for_input[input_index].push_back(output_index);
-
-							input_indices_list_for_filter[filter_index].push_back(input_index);
-							output_indices_list_for_filter[filter_index].push_back(output_index);
 						}
 					}
 				}
 			}
 		}
-		return std::make_tuple(
-			input_indices_list_for_output, filter_indices_list_for_output,
-			output_indices_list_for_input, filter_indices_list_for_input,
-			input_indices_list_for_filter, output_indices_list_for_filter);
+		return std::make_tuple(output_indices_list_for_input, filter_indices_list_for_input);
 	}
 	decltype(auto) concat_indices(std::vector<cpu_indices> const& indices_list) {
 		cpu_indices concatinated;
@@ -65,17 +50,9 @@ namespace neu {
 	}
 
 	struct convolution_indices {
-		gpu_indices indices_range_list_for_output;
-		gpu_indices input_indices_list_for_output;
-		gpu_indices filter_indices_list_for_output;
-
 		gpu_indices indices_range_list_for_input;
 		gpu_indices output_indices_list_for_input;
 		gpu_indices filter_indices_list_for_input;
-
-		gpu_indices indices_range_list_for_filter;
-		gpu_indices input_indices_list_for_filter;
-		gpu_indices output_indices_list_for_filter;
 	};
 
 	decltype(auto) make_convolution_indices(
@@ -83,47 +60,17 @@ namespace neu {
 		auto indices_tuple =
 			neu::calc_convolution_indices(input_width, output_width, filter_width,
 				stride, pad);
-
-		std::cout << "here" << std::get<0>(indices_tuple).size() << std::endl;
-		std::cout << "here" << std::get<1>(indices_tuple).size() << std::endl;
-		std::cout << "here" << std::get<2>(indices_tuple).size() << std::endl;
-		std::cout << "here" << std::get<3>(indices_tuple).size() << std::endl;
-		std::cout << "here" << std::get<4>(indices_tuple).size() << std::endl;
-		std::cout << "here" << std::get<5>(indices_tuple).size() << std::endl;
-		auto indices_range_list_for_output = neu::to_gpu_indices(
+		auto indices_range_list_for_input = neu::to_gpu_indices(
 			neu::make_range_list(std::get<0>(indices_tuple)));
-		auto input_indices_list_for_output= neu::to_gpu_indices(
+		auto output_indices_list_for_input = neu::to_gpu_indices(
 			neu::concat_indices(std::get<0>(indices_tuple)));
-		auto filter_indices_list_for_output = neu::to_gpu_indices(
+		auto filter_indices_list_for_input = neu::to_gpu_indices(
 			neu::concat_indices(std::get<1>(indices_tuple)));
 
-		auto indices_range_list_for_input = neu::to_gpu_indices(
-			neu::make_range_list(std::get<2>(indices_tuple)));
-		auto output_indices_list_for_input = neu::to_gpu_indices(
-			neu::concat_indices(std::get<2>(indices_tuple)));
-		auto filter_indices_list_for_input = neu::to_gpu_indices(
-			neu::concat_indices(std::get<3>(indices_tuple)));
-
-		auto indices_range_list_for_filter = neu::to_gpu_indices(
-			neu::make_range_list(std::get<4>(indices_tuple)));
-		auto input_indices_list_for_filter = neu::to_gpu_indices(
-			neu::concat_indices(std::get<4>(indices_tuple)));
-		auto output_indices_list_for_filter = neu::to_gpu_indices(
-			neu::concat_indices(std::get<5>(indices_tuple)));
-
 		return convolution_indices{
-			std::move(indices_range_list_for_output),
-			std::move(input_indices_list_for_output),
-			std::move(filter_indices_list_for_output),
-
 			std::move(indices_range_list_for_input),
 			std::move(output_indices_list_for_input),
-			std::move(filter_indices_list_for_input),
-
-			std::move(indices_range_list_for_filter),
-			std::move(input_indices_list_for_filter),
-			std::move(output_indices_list_for_filter)
-		};
+			std::move(filter_indices_list_for_input)};
 	}
 
 }// namespace neu
