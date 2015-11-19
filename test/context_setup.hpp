9e03e11a28317fb5c38e42aa3dc04f2ff4de9188
okdshin <kokuzen@gmail.com>
2015-11-19 11:09:39 +0900

    add test/context_setup.hpp

---
+++ test/context_setup.hpp
@@ -0,0 +1,23 @@
+#ifndef BOOST_COMPUTE_TEST_CONTEXT_SETUP_HPP
+#define BOOST_COMPUTE_TEST_CONTEXT_SETUP_HPP
+
+#include <boost/compute/system.hpp>
+#include <boost/compute/command_queue.hpp>
+
+#include "opencl_version_check.hpp"
+
+struct Context {
+    boost::compute::device        device;
+    boost::compute::context       context;
+    boost::compute::command_queue queue;
+
+    Context() :
+        device ( boost::compute::system::default_device() ),
+        context( boost::compute::system::default_context() ),
+        queue  ( boost::compute::system::default_queue() )
+    {}
+};
+
+BOOST_FIXTURE_TEST_SUITE(compute_test, Context)
+
+#endif
