6fa6101851548f24d9e0b43df8af8a79f9eb2e7b
okdshin <kokuzen@gmail.com>
2015-12-17 10:44:22 +0900

    update xor_sigmoid example

--- example/xor_sigmoid/main.cpp
+++ example/xor_sigmoid/main.cpp
@@ -3,26 +3,30 @@
 #include <boost/timer.hpp>
 #include <boost/progress.hpp>
 #include <neu/vector_io.hpp>
-#include <neu/layers_algorithm.hpp>
 #include <neu/kernel.hpp>
 #include <neu/activation_func/sigmoid_loss.hpp>
 #include <neu/activation_func/rectifier.hpp>
 #include <neu/activation_func/sigmoid.hpp>
-#include <neu/activation_layer.hpp>
-//#include <neu/learning_rate_gen/fixed_learning_rate_gen.hpp>
-#include <neu/learning_rate_gen/weight_decay_and_momentum.hpp>
-#include <neu/fully_connected_layer.hpp>
-#include <neu/layer.hpp>
+#include <neu/layer/activation.hpp>
+#include <neu/optimizer/momentum.hpp>
+#include <neu/layer/inner_product.hpp>
+#include <neu/layer/bias.hpp>
+#include <neu/layer/any_layer.hpp>
+#include <neu/layer/any_layer_vector.hpp>
+#include <neu/layer/branch_reduce.hpp>
+#include <neu/layer/load.hpp>
 
 int main(int argc, char** argv) {
 	std::cout << "hello world" << std::endl;
+	auto& queue = boost::compute::system::default_queue();
+	auto context = boost::compute::system::default_context();
 
-	//std::random_device device; std::mt19937 rand(device());
-	std::mt19937 rand(0);
+	std::random_device device; std::mt19937 rand(device());
+	//std::mt19937 rand(0);
 
-	auto input_dim = 2u;
-	auto output_dim = 1u;
-	auto batch_size = 4u;
+	const auto input_dim = 2u;
+	const auto output_dim = 1u;
+	const auto batch_size = 4u;
 
 	std::vector<neu::cpu_vector> cpu_input = {
 		{0.f, 0.f}, {1.f, 0.f}, {0.f, 1.f}, {1.f, 1.f}
@@ -31,22 +35,8 @@ int main(int argc, char** argv) {
 		{0.f}, {1.f}, {1.f}, {0.f}
 	};
 
-	neu::gpu_vector input;
-	for(auto const& cpui : cpu_input) {
-		input.insert(input.end(), cpui.begin(), cpui.end());
-	}
-	neu::gpu_vector teach;
-	for(auto const& cput : cpu_teach) {
-		teach.insert(teach.end(), cput.begin(), cput.end());
-	}
-
-	auto fc1_param = neu::fully_connected_layer_parameter()
-		.input_dim(input_dim).batch_size(batch_size)
-		.output_dim(10);
-	auto relu1_param = neu::make_activation_layer_parameter(fc1_param);
-	auto fc2_param = neu::make_fully_connected_layer_parameter(relu1_param)
-		.output_dim(output_dim);
-	auto sigmoid_loss_param = neu::make_activation_layer_parameter(fc2_param);
+	const auto input = neu::to_gpu_vector(neu::flatten(cpu_input), queue);
+	const auto teach = neu::to_gpu_vector(neu::flatten(cpu_teach), queue);
 
 	auto fc12_g = [&rand, dist=std::normal_distribution<>(0.f, 1.f)]
 		() mutable { return dist(rand); };
@@ -54,51 +44,69 @@ int main(int argc, char** argv) {
 
 	constexpr neu::scalar base_lr = 0.1;
 	constexpr neu::scalar momentum = 0.9;
-	constexpr neu::scalar weight_decay = 0.;
+	constexpr std::size_t hidden_node_num = 10u;
 
-	auto fc1 = neu::make_fully_connected_layer(fc1_param, fc12_g, constant_g,
-		neu::make_weight_decay_and_momentum(base_lr, momentum, weight_decay,
-			fc1_param.weight_dim(), fc1_param.bias_dim()));
-	auto relu1 = neu::make_activation_layer(relu1_param, neu::rectifier());
-	auto fc2 = neu::make_fully_connected_layer(fc2_param, fc12_g, constant_g,
-		neu::make_weight_decay_and_momentum(base_lr, momentum, weight_decay,
-			fc2_param.weight_dim(), fc2_param.bias_dim()));
-	auto sigmoid_loss = neu::make_activation_layer(sigmoid_loss_param,
-		neu::sigmoid_loss());
+	std::vector<neu::layer::any_layer> nn;
+	nn.push_back(neu::layer::make_inner_product(
+		input_dim, hidden_node_num, batch_size, fc12_g,
+		neu::optimizer::momentum(base_lr, momentum, input_dim*hidden_node_num, queue),
+		queue));
+	nn.push_back(neu::layer::make_bias(neu::layer::output_dim(nn), batch_size, constant_g,
+		neu::optimizer::momentum(base_lr, momentum, neu::layer::output_dim(nn), queue),
+		queue));
+	nn.push_back(neu::layer::make_activation(neu::layer::output_dim(nn), batch_size,
+		neu::rectifier()));
+	nn.push_back(neu::layer::make_inner_product(
+		neu::layer::output_dim(nn), output_dim, batch_size, fc12_g,
+		neu::optimizer::momentum(base_lr, momentum,
+			neu::layer::output_dim(nn)*output_dim, queue), queue));
+	nn.push_back(neu::layer::make_bias(neu::layer::output_dim(nn), batch_size, constant_g,
+		neu::optimizer::momentum(base_lr, momentum, neu::layer::output_dim(nn), queue),
+		queue));
+	nn.push_back(neu::layer::make_activation(neu::layer::output_dim(nn), batch_size,
+		neu::sigmoid_loss()));
 
-	auto layers = std::vector<neu::layer>{
-		std::ref(fc1),
-		relu1,
-		std::ref(fc2),
-		sigmoid_loss
-	};
+	neu::gpu_vector output(neu::layer::output_size(nn), context);
+	neu::gpu_vector prev_delta(neu::layer::input_size(nn), context);
+
 	std::ofstream cel_error_log("cel_error.txt");
 	std::ofstream output_log("output.txt");
 	std::ofstream del_weight_log("del_weight.txt");
 
-	neu::gpu_vector output;
-	neu::gpu_vector prev_delta;
-	auto iteration_limit = 100u;
+	const auto iteration_limit = 100u;
 	boost::progress_display progress(iteration_limit);
 	boost::timer timer;
 	for(auto i = 0u; i < iteration_limit; ++i) {
-		neu::layers_forward(layers, input, output);
+		neu::layer::forward(nn, input, output, queue);
 		{
 			neu::print(output_log, output, output_dim);
-			auto cel = neu::cross_entropy_loss(output, teach);
+			auto cel = neu::range::cross_entropy_loss(output, teach);
 			cel_error_log << i << " " << cel << std::endl;
 		}
-		neu::gpu_vector error(output.size());
-		neu::calc_last_layer_delta(output, teach, error);
-		neu::layers_backward(layers, error, prev_delta);
-		neu::layers_update(layers);
-		{
-			//neu::print(del_weight_log, fc1.del_weight(), input_dim);
-			del_weight_log << i << " " << neu::range_sum(fc1.del_weight()) << std::endl;
-		}
+		neu::gpu_vector error(output.size(), context);
+		neu::range::calc_last_layer_delta(output, teach, error, queue);
+		neu::layer::backward(nn, error, prev_delta, false, queue);
+		neu::layer::update(nn, queue);
 		++progress;
 	}
 	std::cout << timer.elapsed() << " sec" << std::endl;
 	neu::print(std::cout, output, output_dim);
 	boost::compute::system::finish();
+	YAML::Emitter emitter;
+	neu::layer::save(nn, emitter, queue);
+	std::cout << emitter.c_str() << std::endl;
+
+	std::ofstream ofs("nn.yaml");
+	ofs << emitter.c_str();
+
+	{
+		auto node = YAML::Load(emitter.c_str());
+		auto loaded = neu::layer::load(node, queue);
+		YAML::Emitter emitter;
+		neu::layer::save(loaded, emitter, queue);
+		std::ofstream ofs("loaded_nn.yaml");
+		ofs << emitter.c_str();
+	}
+
+
 }
