f3ea2cf4ae3d4fdcc2c9240a78211684b46ec2c0
okdshin <kokuzen@gmail.com>
2015-12-21 11:05:57 +0900

    update max_pooling_layer

--- example/max_pooling_layer/main.cpp
+++ example/max_pooling_layer/main.cpp
@@ -1,36 +1,33 @@
-#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 #include <iostream>
-#include <neu/max_pooling_layer.hpp>
+#include <neu/layer/max_pooling.hpp>
 #include <neu/image.hpp>
 #include <neu/kernel.hpp>
 
 int main() {
 	std::cout << "hello world" << std::endl;
+	auto& queue = boost::compute::system::default_queue();
+	auto context = boost::compute::system::default_context();
+
 	auto batch_size = 1u;
 	neu::gpu_vector input;
 	for(auto b = 0u; b < batch_size; ++b) {
-		auto input_image = neu::load_rgb_image_as_3ch_image_vector("../../../data/lena512.bmp");
+		auto input_image =
+			neu::load_rgb_image_as_3ch_image_vector("../../../data/lena512.bmp");
 		auto cpu_input = std::get<0>(input_image);
 		input.insert(input.end(), cpu_input.begin(), cpu_input.end());
 	}
 
-	neu::max_pooling_layer_parameter params;
-	params
-	.input_width(512)
-	.filter_width(7)
-	.input_channel_num(3)
-	.stride(2)
-	.pad(3)
-	.batch_size(batch_size)
-	;
-	auto pool = neu::make_max_pooling_layer(params);
-	neu::gpu_vector output(neu::layer_output_dim(pool)*neu::layer_batch_size(pool));
-	neu::layer_forward(pool, input, output);
-	neu::save_3ch_image_vector_as_rgb_image(neu::to_cpu_vector(output),
-		neu::layer_output_width(pool), "output.bmp", 255);
+	neu::layer::geometric_layer_property glp{512, 7, 3, 3, 2, 3};
+	auto pool = neu::layer::max_pooling(glp, batch_size, context);
 
-	neu::gpu_vector prev_delta(neu::layer_input_dim(pool)*neu::layer_batch_size(pool));
-	neu::layer_backward(pool, neu::gpu_vector(output.size(), 1.f), prev_delta);
-	neu::save_3ch_image_vector_as_rgb_image(neu::to_cpu_vector(prev_delta),
-		neu::layer_input_width(pool), "prev_delta.bmp", 255);
+	neu::gpu_vector output(neu::layer::whole_output_size(pool));
+	neu::layer::forward(pool, input, output, queue);
+	neu::save_3ch_image_vector_as_rgb_image(neu::to_cpu_vector(output, queue),
+		neu::layer::output_width(pool), "output.bmp", 255);
+	
+	neu::gpu_vector prev_delta(neu::layer::whole_input_size(pool));
+	neu::layer::backward(pool, neu::gpu_vector(output.size(), 1.f), prev_delta, queue);
+	neu::save_3ch_image_vector_as_rgb_image(neu::to_cpu_vector(prev_delta, queue),
+		neu::layer::input_width(pool), "prev_delta.bmp", 255);
+
 }
--- neu/layer/max_pooling/impl.hpp
+++ neu/layer/max_pooling/impl.hpp
@@ -1,116 +1,158 @@
-#ifndef NEU_MAX_POOLING_LAYER_IMPL_HPP
-#define NEU_MAX_POOLING_LAYER_IMPL_HPP
+#ifndef NEU_LAYER_MAX_POOLING_IMPL_HPP
+#define NEU_LAYER_MAX_POOLING_IMPL_HPP
 //20151026
 #include <neu/assert.hpp>
 #include <neu/validation.hpp>
 #include <neu/basic_type.hpp>
-#include <neu/range_traits.hpp>
-#include <neu/range_algorithm.hpp>
-#include <neu/layer_traits.hpp>
+#include <neu/range/traits.hpp>
+#include <neu/range/algorithm.hpp>
+#include <neu/layer/traits.hpp>
 #include <neu/kernel.hpp>
-#include <neu/max_pooling_layer/kernel_source.hpp>
+#include <neu/layer/geometric_layer_property.hpp>
+#include <neu/layer/max_pooling/kernel_source.hpp>
 
 namespace neu {
-	class max_pooling_layer {
-	public:
-		using layer_category = convolution_like_layer_tag;
+	namespace layer {
+		class max_pooling {
+		public:
+			max_pooling() = default;
 
-		max_pooling_layer() = default;
+			max_pooling(
+				geometric_layer_property const& glp,
+				std::size_t batch_size,
+				boost::compute::context const& context)
+			: glp_(glp), batch_size_(batch_size),
+			pooling_kernel_(make_kernel(max_pooling_kernel_source,
+				"max_pooling", context)),
+			output_width_(output_width(glp)),
+			gpu_indices_(output_dim(glp)*batch_size_, context) {}
 
-		max_pooling_layer(std::size_t input_width, std::size_t output_width,
-			std::size_t filter_width,
-			std::size_t input_channel_num, std::size_t stride, std::size_t batch_size,
-			kernel const& pooling_kernel,
-			boost::compute::context const& context)
-			: input_width_(input_width), filter_width_(filter_width),  
-			input_channel_num_(input_channel_num),
-			stride_(stride), batch_size_(batch_size),
-			pooling_kernel_(pooling_kernel),
-			output_width_(output_width),
-			gpu_indices_(output_width*output_width*input_channel_num*batch_size, context) {}
+			decltype(auto) batch_size() const { return batch_size_; }
 
-		decltype(auto) input_width() const { return input_width_; }
-		decltype(auto) input_channel_num() const { return input_channel_num_; }
-		decltype(auto) output_width() const { return output_width_; }
-		// output channel num is equal to the of input
-		decltype(auto) output_channel_num() const { return input_channel_num_; }
-		decltype(auto) batch_size() const { return batch_size_; }
+			decltype(auto) input_rank() const { return 3; }
+			decltype(auto) output_rank() const { return 3; }
 
-		template<typename InputRange, typename OutputRange>
-		decltype(auto) test_forward(std::size_t test_batch_size,
-				InputRange const& input, OutputRange const& output,
-				boost::compute::command_queue& queue
-					=boost::compute::system::default_queue()) {
-			NEU_ASSERT(neu::range_distance(input) ==
-				neu::layer_input_dim(*this)*test_batch_size);
-			NEU_ASSERT(neu::range_distance(output) ==
-				neu::layer_output_dim(*this)*test_batch_size);
-			NEU_ASSERT_FOR_HEAVY_CALCULATION(is_all_of_finite(input, queue));
-			enqueue_nd_range_kernel<3>(queue, pooling_kernel_,
-				{0, 0, 0}, {output_width_, output_width_, test_batch_size},
-				neu::range_get_buffer(input),
-				static_cast<int>(neu::range_get_begin_index(input)),
-				neu::range_get_buffer(output),
-				static_cast<int>(neu::range_get_begin_index(output)),
-				gpu_indices_,
-				static_cast<int>(stride_), static_cast<int>(input_channel_num_),
-				static_cast<int>(input_width_), static_cast<int>(output_width_),
-				static_cast<int>(filter_width_));
-			NEU_ASSERT_FOR_HEAVY_CALCULATION(is_all_of_finite(output, queue));
-		}
-
-		template<typename InputRange, typename OutputRange>
-		decltype(auto) backward(InputRange const& delta, OutputRange const& prev_delta,
-				boost::compute::command_queue& queue
-					=boost::compute::system::default_queue()) {
-			NEU_ASSERT(neu::range_distance(delta) == neu::layer_output_size(*this));
-			NEU_ASSERT(neu::range_distance(prev_delta) == neu::layer_input_size(*this));
-			NEU_ASSERT_FOR_HEAVY_CALCULATION(is_all_of_finite(delta, queue));
-
-			cpu_indices indices(gpu_indices_.size());
-			neu::range_copy(gpu_indices_, indices, queue);
-
-			cpu_vector cpu_delta(neu::range_distance(delta));
-			neu::range_copy(delta, cpu_delta, queue);
-
-			cpu_vector cpu_prev_delta(neu::range_distance(prev_delta), 0.f);
-			for(auto i = 0u; i < indices.size(); ++i) {
-				NEU_ASSERT(i < indices.size());
-				NEU_ASSERT(i < cpu_delta.size());
-				NEU_ASSERT(static_cast<std::size_t>(indices[i]) < cpu_prev_delta.size());
-				cpu_prev_delta[indices[i]] += cpu_delta[i];
+			decltype(auto) input_size(rank_id ri) const {
+				return ri == rank_id::width || ri == rank_id::height ? glp_.input_width :
+					ri == rank_id::channel_num ? glp_.input_channel_num : 0;
 			}
-			neu::range_copy(cpu_prev_delta, prev_delta, queue);
+			decltype(auto) output_size(rank_id ri) const {
+				return ri == rank_id::width || ri == rank_id::height ? output_width_ :
+					ri == rank_id::channel_num ? glp_.output_channel_num : 0;
+			}
 
-			NEU_ASSERT_FOR_HEAVY_CALCULATION(is_all_of_finite(prev_delta, queue));
+			template<typename InputRange, typename OutputRange>
+			decltype(auto) test_forward(std::size_t test_batch_size,
+					InputRange const& input, OutputRange const& output,
+					boost::compute::command_queue& queue) {
+				NEU_ASSERT(neu::range::distance(input) ==
+					neu::layer::input_dim(*this)*test_batch_size);
+				NEU_ASSERT(neu::range::distance(output) ==
+					neu::layer::output_dim(*this)*test_batch_size);
+				NEU_ASSERT_FOR_HEAVY_CALCULATION(is_all_of_finite(input, queue));
+
+				enqueue_nd_range_kernel<3>(queue, pooling_kernel_,
+					{0, 0, 0}, {output_width_, output_width_, test_batch_size},
+					neu::range::get_buffer(input),
+					static_cast<int>(neu::range::get_begin_index(input)),
+					neu::range::get_buffer(output),
+					static_cast<int>(neu::range::get_begin_index(output)),
+					gpu_indices_,
+					static_cast<int>(glp_.stride),
+					static_cast<int>(glp_.input_channel_num),
+					static_cast<int>(glp_.input_width), static_cast<int>(output_width_),
+					static_cast<int>(glp_.filter_width)); //TODO pad
+
+				NEU_ASSERT_FOR_HEAVY_CALCULATION(is_all_of_finite(output, queue));
+			}
+			
+			template<typename InputRange, typename OutputRange>
+			decltype(auto) forward(
+					InputRange const& input, OutputRange const& output,
+					boost::compute::command_queue& queue) {
+				NEU_ASSERT(neu::range::distance(input) ==
+					neu::layer::whole_input_size(*this));
+				NEU_ASSERT(neu::range::distance(output) ==
+					neu::layer::whole_output_size(*this));
+				NEU_ASSERT_FOR_HEAVY_CALCULATION(is_all_of_finite(input, queue));
+
+				test_forward(batch_size_, input, output, queue);
+			}
+
+			template<typename InputRange>
+			decltype(auto) backward_top(
+					InputRange const& delta,
+					boost::compute::command_queue& queue) {
+				/* do nothing */
+			}
+
+			template<typename InputRange, typename OutputRange>
+			decltype(auto) backward(
+					InputRange const& delta, OutputRange const& prev_delta,
+					boost::compute::command_queue& queue) {
+				NEU_ASSERT(neu::range::distance(delta)
+					== neu::layer::whole_output_size(*this));
+				NEU_ASSERT(neu::range::distance(prev_delta)
+					== neu::layer::whole_input_size(*this));
+				NEU_ASSERT_FOR_HEAVY_CALCULATION(is_all_of_finite(delta, queue));
+
+				const auto cpu_indices = to_cpu_indices(gpu_indices_, queue);
+
+				const auto cpu_delta = [&delta, &queue](){
+					cpu_vector cpu_delta(neu::range::distance(delta));
+					neu::range::copy(delta, cpu_delta, queue);
+					return cpu_delta;
+				}();
+
+				cpu_vector cpu_prev_delta(neu::range::distance(prev_delta), 0.f);
+				for(auto i = 0u; i < cpu_indices.size(); ++i) {
+					NEU_ASSERT(i < cpu_indices.size());
+					NEU_ASSERT(i < cpu_delta.size());
+					NEU_ASSERT(static_cast<std::size_t>(
+						cpu_indices[i]) < cpu_prev_delta.size());
+
+					cpu_prev_delta[cpu_indices[i]] += cpu_delta[i];
+				}
+				neu::range::copy(cpu_prev_delta, prev_delta, queue);
+
+				NEU_ASSERT_FOR_HEAVY_CALCULATION(is_all_of_finite(prev_delta, queue));
+			}
+
+			decltype(auto) update(boost::compute::command_queue&) { /* do nothing */ }
+
+			decltype(auto) serialize(
+					YAML::Emitter& emitter, boost::compute::command_queue& queue) const {
+				emitter << YAML::BeginMap
+					<< YAML::Key << "layer_type"
+						<< YAML::Value << "max_pooling";
+				layer::serialize(glp_, emitter);
+				emitter 
+					<< YAML::Key << "output_width(calculated)"
+						<< YAML::Value << output_width_
+					<< YAML::Key << "batch_size"
+						<< YAML::Value << batch_size_
+				<< YAML::EndMap;
+			}
+
+		private:
+			geometric_layer_property glp_;
+
+			std::size_t batch_size_;
+
+			kernel pooling_kernel_;
+
+			std::size_t output_width_;
+
+			gpu_indices gpu_indices_;
+		};
+
+		decltype(auto) deserialize_max_pooling(YAML::Node const& node,
+				boost::compute::context const& context) {
+			const auto glp = deserialize_geometric_layer_property(node);
+			return max_pooling(
+				glp, node["batch_size"].as<std::size_t>(), context);
 		}
-
-	private:
-		std::size_t input_width_;
-		std::size_t filter_width_;
-		std::size_t input_channel_num_;
-		std::size_t stride_;
-		std::size_t batch_size_;
-
-		kernel pooling_kernel_;
-
-		std::size_t output_width_;
-
-		gpu_indices gpu_indices_;
-	};
-	decltype(auto) make_max_pooling_layer(
-		std::size_t input_width, std::size_t output_width,
-		std::size_t filter_width,
-		std::size_t input_channel_num, std::size_t stride, std::size_t batch_size,
-		boost::compute::context const& context
-			=boost::compute::system::default_context()
-	){
-		auto pooling_kernel = make_kernel(max_pooling_kernel_source,
-			"max_pooling", context);
-		return max_pooling_layer(input_width, output_width, filter_width,
-			input_channel_num, stride, batch_size, pooling_kernel,
-			context);
 	}
 }// namespace neu
 
-#endif //NEU_MAX_POOLING_LAYER_IMPL_HPP
+#endif //NEU_LAYER_MAX_POOLING_IMPL_HPP
