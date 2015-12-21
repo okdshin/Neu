feada7b40e08cc98b35cbcea63a57e9f5ec4d9db
okdshin <kokuzen@gmail.com>
2015-12-21 11:01:17 +0900

    update average_pooling_layer example

--- example/average_pooling_layer/main.cpp
+++ example/average_pooling_layer/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-#include <neu/average_pooling_layer.hpp>
+#include <neu/layer/average_pooling.hpp>
 #include <neu/image.hpp>
 #include <neu/kernel.hpp>
 
 int main() {
 	std::cout << "hello world" << std::endl;
+	auto& queue = boost::compute::system::default_queue();
+	auto context = boost::compute::system::default_context();
+
 	auto batch_size = 1u;
 	neu::gpu_vector input;
 	for(auto b = 0u; b < batch_size; ++b) {
@@ -14,6 +17,20 @@ int main() {
 		input.insert(input.end(), cpu_input.begin(), cpu_input.end());
 	}
 
+	neu::layer::geometric_layer_property glp{512, 20, 3, 3, 2, 10};
+	auto pool = neu::layer::make_uniform_average_pooling(glp, batch_size, queue);
+
+	neu::gpu_vector output(neu::layer::whole_output_size(pool));
+	neu::layer::forward(pool, input, output, queue);
+	neu::save_3ch_image_vector_as_rgb_image(neu::to_cpu_vector(output, queue),
+		neu::layer::output_width(pool), "output.bmp", 255);
+	
+	neu::gpu_vector prev_delta(neu::layer::whole_input_size(pool));
+	neu::layer::backward(pool, neu::gpu_vector(output.size(), 1.f), prev_delta, queue);
+	neu::save_3ch_image_vector_as_rgb_image(neu::to_cpu_vector(prev_delta, queue),
+		neu::layer::input_width(pool), "prev_delta.bmp", 255);
+
+	/*
 	neu::average_pooling_layer_parameter params;
 	params
 	.input_width(512)
@@ -33,4 +50,5 @@ int main() {
 	neu::layer_backward(pool, neu::gpu_vector(output.size(), 1.f), prev_delta);
 	neu::save_3ch_image_vector_as_rgb_image(neu::to_cpu_vector(prev_delta),
 		neu::layer_input_width(pool), "prev_delta.bmp", 255);
+	*/
 }
