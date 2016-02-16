355922bce845fb72eedc8ababa18a66838002577
okdshin <kokuzen@gmail.com>
2016-02-17 05:40:38 +0900

    implemented batch_normalization layer without test_forward

---
+++ neu/layer/batch_normalization.hpp
@@ -0,0 +1,5 @@
+#ifndef NEU_LAYER_BATCH_NORMALIZATION_HPP
+#define NEU_LAYER_BATCH_NORMALIZATION_HPP
+//20160212
+#include <neu/layer/batch_normalization/impl.hpp>
+#endif //NEU_LAYER_BATCH_NORMALIZATION_HPP
